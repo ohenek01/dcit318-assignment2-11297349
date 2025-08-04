f70dd6e0dd92a0525f94791bdbfe765c0b108086
ohenek01 <144062701+ohenek01@users.noreply.github.com>
2025-08-04 18:13:42 +0000

    added the interfaces class

---
+++ Assignment2/Interfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("The car is moving");
+    }
+}
+
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("The bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        car.Move();
+        bicycle.Move();
+    }
+}
