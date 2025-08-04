9890a65d0e97014399f55cb7da2a7e21989f9f55
ohenek01 <144062701+ohenek01@users.noreply.github.com>
2025-08-04 17:49:20 +0000

    added inheritance and overriding and the abstract classes

---
+++ Assignment2/AbstractClasses/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double Radius)
+    {
+        this.Radius = Radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double Width, double Height)
+    {
+        this.Width = Width;
+        this.Height = Height;
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(8, 4);
+
+        Console.WriteLine("Circle Area: " + circle.GetArea());
+        Console.WriteLine("Rectangle Area: " + rectangle.GetArea());
+    }
+}
